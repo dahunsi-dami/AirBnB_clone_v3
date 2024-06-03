1d9a44d22e281425553b50859c802a1a0eb1cd06
Itohan <momoduitohan5@gmail.com>
2024-06-03 12:52:49 +0100

    states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -45,7 +45,6 @@ def create_name():
         abort(400, "Missing name")
 
     new_state = State(**data)
-    storage.new(new_state)
     storage.save()
 
     return jsonify(new_state.to_dict()), 201

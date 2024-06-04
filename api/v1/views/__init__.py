b2f49496a302233aaa610de0bb6c36d09d4c0398
Itohan <momoduitohan5@gmail.com>
2024-06-04 16:05:49 +0100

    these lines

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,8 @@
 """Your first endpoint (route) will be to return the status of your API"""
 
 from flask import Blueprint
+
+
 app_views = Blueprint('/api/v1', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *

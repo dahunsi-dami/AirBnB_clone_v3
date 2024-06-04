db7e2d524b7737578d098332a3c774aac9ad9d07
Itohan <momoduitohan5@gmail.com>
2024-06-04 12:51:27 +0100

    places

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,7 @@
 
 from flask import Blueprint
 app_views = Blueprint('/api/v1', __name__, url_prefix='/api/v1')
+city_views = Blueprint('cities', __name__, url_prefix='/api/v1/cities')
 
 from api.v1.views.index import *
 from api.v1.views.states import *

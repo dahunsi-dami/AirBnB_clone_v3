5ef537e72304aa61b4d71305a405b9502b43c6f7
Itohan <momoduitohan5@gmail.com>
2024-06-04 10:39:54 +0100

    cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -5,7 +5,7 @@ Same as State, create a new view for City
 from flask import abort, Flask, jsonify, request
 from api.v1.views import app_views
 from models import storage
-from models.city import City
+from models.state import State
 
 
 @app_views.route("/states/<string:state_id>/cities",
